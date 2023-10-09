a9df3a70c604ccf187291f4d7e3150a57a9aaa15
igor_dubchuk <igor.dubchuk@gmail.com>
2023-10-09 13:25:59 +0400

    Добавлена библиотечка для обработки запросов апи

---
+++ ApiRequestProcessor.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CommonLibrary
+{
+    public static class ApiRequestProcessor
+    {
+        public static IActionResult ProcessApiRequest(Action processApiRequest, ILogger logger)
+        {
+            try
+            {
+                processApiRequest();
+                return new StatusCodeResult(200);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error while API request processing.");
+                return new ObjectResult(ex.Message)
+                {
+                    StatusCode = 500,
+                };
+            }
+        }
+        public static ActionResult<ResponseType> ProcessApiRequest<ResponseType>(Func<ResponseType> processApiRequest, ILogger logger)
+        {
+            try
+            {
+                var result = processApiRequest();
+                return new ObjectResult(result)
+                {
+                    StatusCode = 200,
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error while API request processing.");
+
+                var exType = ex.GetType().ToString();
+                short statusCode = exType switch
+                {
+                    "System.ApplicationException" => 500,
+                    _ => 500
+                };
+                return new ObjectResult(ex.Message)
+                {
+                    StatusCode = statusCode,
+                };
+            }
+        }
+    }
+}
